75dcf9e54a89c5ea203a86872c81df8d3f89d0d6
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2022-10-03 13:31:36 +0530

    Create status.py

---
+++ status.py
@@ -0,0 +1,28 @@
+from jenkinsapi.jenkins import Jenkins
+from jenkinsapi.custom_exceptions import NoBuildData
+from requests import ConnectionError
+
+def get_job_by_build_state(url, view_name, state='FAILURE'):
+    server = Jenkins(url)
+    view_url = f'{url}/view/{view_name}/'
+    view = server.get_view_by_url(view_url)
+    jobs = view.get_job_dict()
+
+    jobs_by_state = []
+
+    for job in jobs:
+        job_url = f'{url}/{job}'
+        j = server.get_job(job)
+        try:
+            build = j.get_last_completed_build()
+            status = build.get_status()
+            if status == state:
+                jobs_by_state.append(job)
+                except NoBuildData:
+                    continue
+                except ConnectionError
+                    pass
+    return jobs_by_state
+
+if __name__ == '__main__':
+    jobs = get_job_by_build_state(url='', view_name='VIEW_NAME', state='UNSTABLE')
