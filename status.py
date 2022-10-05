f9561df20352eff2736fa637b3c7d41137044037
Uday <102657313+Kalla-Udaykumar@users.noreply.github.com>
2022-10-05 10:03:50 +0530

    Update status.py

--- status.py
+++ status.py
@@ -18,10 +18,8 @@ def get_job_by_build_state(url, view_name, state='FAILURE'):
             status = build.get_status()
             if status == state:
                 jobs_by_state.append(job)
-              #  except NoBuildData :
-               #     continue
-                #except ConnectionError :
-                 #   pass
+                except BuildFailure
+                    continue
     return jobs_by_state
 
 if __name__ == '__main__':
